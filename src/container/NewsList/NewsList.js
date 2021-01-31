c1ddfd601de5091ec23a2ec9fef07736856fd848
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2021-01-31 11:36:49 -0300

    add button to signup

--- src/container/NewsList/NewsList.js
+++ src/container/NewsList/NewsList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 import { styles } from './NewsListStyles';
 
-const NewsList = () => {
+const NewsList = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>
         News List
       </Text>
+      <Button
+        title="Cadastro"
+        onPress={() => navigation.navigate('Cadastro')}
+      />
     </View>
   );
 };
