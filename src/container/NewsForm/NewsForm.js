23007fa8d0749a531320ba5e3cd6443da61cf386
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2021-02-05 19:44:46 -0300

    add styles

--- src/container/NewsForm/NewsForm.js
+++ src/container/NewsForm/NewsForm.js
@@ -1,15 +1,69 @@
 import React from 'react';
+import { Text, View, TextInput, Button } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
 
-import { View, Text } from 'react-native';
+import styles from './NewsFormStyles';
 
-import { styles } from './NewsFormStyles';
+const NewsForm = props => {
+  const { control, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
 
-const NewsForm = () => {
   return (
     <View style={styles.container}>
-      <Text>
-        News Form
-      </Text>
+      <Text style={styles.titleStyle}>Cadastro de Noticias</Text>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <>
+            <Text style={styles.labelStyle}>Titulo</Text>
+            <TextInput
+              style={styles.inputTitle}
+              onBlur={onBlur}
+              onChangeText={value => onChange(value)}
+              value={value}
+            />
+          </>
+        )}
+        name="title"
+        defaultValue=""
+      />
+
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <>
+            <Text style={styles.labelStyle}>Noticia</Text>
+            <TextInput
+              style={styles.inputDescriptionStyle}
+              onBlur={onBlur}
+              multiline
+              onChangeText={value => onChange(value)}
+              value={value}
+            />
+          </>
+        )}
+        name="description"
+        defaultValue=""
+      />
+
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <>
+            <Text style={styles.labelStyle}>Autor</Text>
+            <TextInput
+              style={styles.inputAutor}
+              onBlur={onBlur}
+              onChangeText={value => onChange(value)}
+              value={value}
+            />
+          </>
+        )}
+        name="autor"
+        defaultValue=""
+      />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 };
--- src/container/NewsForm/NewsFormStyles.js
+++ src/container/NewsForm/NewsFormStyles.js
@@ -1,9 +1,56 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
-export const styles = StyleSheet.create({
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     textAlign: 'center',
-  }
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+  titleStyle: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 40,
+    marginBottom: 20,
+  },
+  labelStyle: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  inputTitle: {
+    borderColor: 'white',
+    borderWidth: 1,
+    width: windowWidth * 0.8,
+    color: 'white',
+    marginBottom: 20,
+    height: windowHeight * 0.05,
+    fontSize: 20,
+  },
+  inputDescriptionStyle: {
+    borderColor: 'white',
+    borderWidth: 1,
+    fontSize: 20,
+    padding: 10,
+    width: windowWidth * 0.9,
+    height: windowHeight * 0.4,
+    color: 'white',
+    marginBottom: 20,
+  },
+  inputAutor: {
+    borderColor: 'white',
+    borderWidth: 1,
+    width: windowWidth * 0.8,
+    height: windowHeight * 0.05,
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 20,
+  },
 });
+
+export default styles;
